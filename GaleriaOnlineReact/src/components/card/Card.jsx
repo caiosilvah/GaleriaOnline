f5eb0e73112b516b352ffdc1537b71ffe0bb4123
Subindo a recuperacao para o Git <caio.p.silva8@senail.aluno.br>
2025-08-26 14:14:35 -0300

    feat: finalizando o editar e excluir

--- GaleriaOnlineReact/src/components/card/Card.jsx
+++ GaleriaOnlineReact/src/components/card/Card.jsx
@@ -1,18 +1,16 @@
 import "./Card.css"
-import imgCard from '../../assets/img/porschepanamera.png'
-
 import imgPen from '../../assets/img/pen.svg'
 import imgTrash from '../../assets/img/trash.svg'
 
-export const Card = ({tituloCard}) => {
+export const Card = ({tituloCard,imgCard, funcaoEditar, funcaoExcluir}) => {
     return (
         <>
             <div className="cardDaImagem">
                 <p>{tituloCard}</p>
                 <img className="imgDoCard" src={imgCard} alt="Imagem relacionada ao card."/>
                 <div className="icons">
-                    <img src={imgPen} alt="Icone de caneta para realizar uma alteração."/>
-                    <img src={imgTrash} alt="Icone de uma lixeira para realizar a exclusão."/>
+                    <img src={imgPen} onClick={funcaoEditar} alt="Icone de caneta para realizar uma alteração."/>
+                    <img src={imgTrash} onClick={funcaoExcluir} alt="Icone de uma lixeira para realizar a exclusão."/>
                 </div>
             </div>
         </>
--- GaleriaOnlineReact/src/pages/Galeria/Galeria.jsx
+++ GaleriaOnlineReact/src/pages/Galeria/Galeria.jsx
@@ -1,39 +1,144 @@
-import './Galeria.css'
-import icon from "./../../assets/img/upload.svg"
-import { Botao } from '../../components/botao/Botao'
-import { Card } from '../../components/card/Card'
-
+import './Galeria.css';
+import icon from "../../assets/img/upload.svg";
+import { Botao } from '../../components/botao/Botao';
+import { Card } from '../../components/card/Card';
+import { useEffect, useState } from 'react';
+import api from '../../Services/services';
 
 export const Galeria = () => {
-    return(
+    const [cards, setCards] = useState([]);
+    const [imagem, setImagem] = useState(null);
+    const [nomeImagem, setNomeImagem] = useState("");
+
+    async function listarCards() {
+        try {
+            const resposta = await api.get("Imagem");
+            setCards(resposta.data);
+        } catch (error) {
+            console.error(error);
+            alert("Erro ao listar.");
+        }
+    }
+
+    async function cadastrarCard(e) {
+
+        e.preventDefault();
+
+        if (imagem && nomeImagem) {
+            try {
+                //FormData e uma interface JavaScript Que permite construir um conjunto de pares chave/valor representando os dados de um formulario HTML.
+                const formData = new FormData();
+                //append: anexar/acrescentar/adicionar
+                formData.append("Nome", nomeImagem);
+                formData.append("Arquivo", imagem);
+
+                await api.post("Imagem/upload", formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                });
+
+                alert("Pabens, seu cadastro foi realizado!!!")
+
+            } catch (error) {
+                alert("Não foi possivel realizar o cadastro!")
+                console.error(error);
+            }
+        } else {
+            alert("Preencha os campos de Nome e Imagem!")
+        }
+    }
+
+    function editarCard(id, nomeAntigo) {
+        try {
+            const novoNome = prompt("Digite o novo nome da imagem:", nomeAntigo);
+
+            const inputArquivo = document.createElement("input");
+            inputArquivo.type = "file";
+            inputArquivo.accept = "image/*";
+            //<input>= "file" accepet="image/*"</input>
+
+            //Define o que acontece quando o usuario selecionar um arquivo
+            inputArquivo.onchange = async (evento) => {
+                const novoArquivo = e.target.files[0];
+
+                const formData = new FormData();
+                //Adicionar o novo nome no formData:
+                formData.append("Nome", novoNome);
+                formData.append("Arquivo", novoArquivo);
+
+                if (formData) {
+                    try {
+                        await api.put(`Imagem/${id}`, formData, {
+                            headers: {
+                                "Content-Type": "multipart/form-data"
+
+                            }
+                        })
+                        alert("ebaa deu certo!")
+                        listarCards();
+                    } catch (error) {
+                        alert("Não foi possivel alterar o card !")
+                        console.error(error);
+                    }
+                }
+            };
+
+        } catch (error) {
+            alert("Não foi possivel editar o alert.")
+            console.error(error);
+
+        }
+    }
+
+    async function excluirCard(id) {
+        try {
+            await api.delete(`Imagem/${id}`);
+            alert("Excluiuuuuu!!!");
+        } catch (error) {
+            alert("Erro ao excluir o card!");
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        listarCards();
+    });
+
+    return (
         <>
-            <h1 className="tituloGaleria">Galeria Online</h1>
-            <form className="formulario" onSubmit="">
-                <div className="campoNome">
+            <h1 className='tituloGaleria'>Galeria Online</h1>
+            <form className='formulario' onSubmit={cadastrarCard}>
+                <div className='campoNome'>
                     <label>Nome</label>
-                    <input type="text" className='inputNome'/>
+                    <input type="text" className='inputNome' onChange={(e) => setNomeImagem(e.target.value)} value={nomeImagem} />
                 </div>
-                <div className="campoImagem">
+                <div className='campoImagem'>
                     <label className='arquivoLabel'>
-                        <i><img src={icon} alt='Icone de upload de imagem'/></i>
-                        <input type='file' className='arquivoInput'/>
+                        <i>
+                            <img src={icon} alt="Ícone de upload de imagem" />
+                        </i>
+                        <input type="file" className="arquivoInput" onChange={(e) => setImagem(e.target.files[0])} />
                     </label>
                 </div>
-                <Botao nomeBotao="Cadastrar"/>
+                <Botao nomeBotao="Cadastrar" />
             </form>
 
             <div className='campoCards'>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
-                <Card tituloCard="Porsche"/>
+                {cards.length > 0 ? (
+                    cards.map((e) => (
+                        <Card
+                            key={e.id}
+                            tituloCard={e.nome}
+                            imgCard={`https://localhost:7009/${e.caminho.replace("wwwroot/", "")}`}
+                            funcaoExcluir={() => excluirCard(e.id)}
+                            funcaoEditar={() => editarCard(e.id, e.nome)}
+                        />
+                    ))
+                ) : (
+                    <p>Nenhum Card cadastrado</p>
+                )}
             </div>
         </>
-    )
-}
+    );
+};
